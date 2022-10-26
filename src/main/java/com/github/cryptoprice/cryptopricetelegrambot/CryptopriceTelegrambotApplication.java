3edf8457ebb184db66bd9289032d1280adf0c041
Grigory Legostaev <grig03@bk.ru>
2022-10-27 00:54:43 +0300

    add SpringBoot

---
+++ src/main/java/com/github/cryptoprice/cryptopricetelegrambot/CryptopriceTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.cryptoprice.cryptopricetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptopriceTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptopriceTelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cryptoprice/cryptopricetelegrambot/CryptopriceTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cryptoprice.cryptopricetelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptopriceTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
